e9d73800b6b0eb423c56d640d8dc8727e8b2a8b1
EnriqueR07 <erubio20672@iesjoanramis.org>
2024-10-22 12:54:21 +0200

    Add files via upload

--- Lista de tareas/js/script.js
+++ Lista de tareas/js/script.js
@@ -1,23 +1,37 @@
-import { initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import{getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const input = document.getElementById("inputField");
-const boto = document.getElementById("añadir");
-const lista = document.getElementById("lista")
-const appConfiguracio = {
+const boto = document.getElementById("afegir");
+const lista = document.getElementById("llista");
+
+const appSettings = {
     databaseURL: "https://lista-de-tareas-142a0-default-rtdb.europe-west1.firebasedatabase.app/"
 }
-
-const app = initializeApp(appConfiguracio);
+const app = initializeApp(appSettings);
 const baseDades = getDatabase(app);
-const tasks = ref(baseDades , "lista");
+const tasks = ref(baseDades, "tareas");
 
 
-
-boto.addEventListener("click", function(){
+boto.addEventListener("click", function () {
     push(tasks, input.value)
-    lista.innerHTML += `<li>${input.value}</li>`
-    input.value= ""
+    addElement(input);
+    clearScreen();
 
 })
 
+function addElement(e) {
+    lista.innerHTML += `<li>${e}</li>`;
+}
 
+function clearScreen() {
+    input.value = ""
+}
+
+onValue(tasks, function (snapshot) {
+    let resultats = Object.values(snapshot.val())
+    for (let i = 0; i < resultats.length; i++) {
+        let current = resultats[i]
+        addElement(current)
+    }
+})
