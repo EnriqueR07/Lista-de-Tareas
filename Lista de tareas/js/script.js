25c94aa0a686a01911eb3a35850f8de2dda67ea2
EnriqueR07 <erubio20672@iesjoanramis.org>
2024-10-25 10:45:39 +0200

    Add files via upload

--- Lista de tareas/js/script.js
+++ Lista de tareas/js/script.js
@@ -1,45 +1,50 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const input = document.getElementById("inputField");
 const boto = document.getElementById("afegir");
 const lista = document.getElementById("llista");
 
 const appSettings = {
-    databaseURL: "https://lista-de-tareas-142a0-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+      databaseURL: "https://addtocart-3079a-default-rtdb.europe-west1.firebasedatabase.app/"
+     }
 const app = initializeApp(appSettings);
 const baseDades = getDatabase(app);
 const tasks = ref(baseDades, "tareas");
 
-
-boto.addEventListener("click", function () {
-    push(tasks, input.value)
-    clearScreen();
-
+boto.addEventListener("click", function() {
+      push (tasks, input.value)
+      Clearscreen();
 })
 
 function addElement(e) {
-let elementLlista = document.createElement("li");
-elementLlista.id=e[0]
-elementLlista.textContent=e[1];
-lista.append(elementLlista);
+
+      let elementLlista = document.createElement("li");
+      elementLlista.id=e[0]
+      elementLlista.textContent=e[1]
+      elementLlista.addEventListener("click", function(){
+            let localitzacioItem = ref(baseDades, `tareas/${e[0]}`)
+            remove(localitzacioItem)
+      }
+)
+      lista.append(elementLlista)
+     
 }
 
-function clearScreen() {
-    input.value = ""
+function Clearscreen(){
+      input.value = ""
 }
-
-function clearlist(){
-    lista.innerHTML=""
+function ClearList(){
+      lista.innerHTML = "";
 }
-
 onValue(tasks, function (snapshot) {
-    let resultats = Object.entries(snapshot.val())
-    clearlist();
-    for (let i = 0; i < resultats.length; i++) {
-      
-        addElement(resultats[i])
-    }
+      if (snapshot.exists()){
+      let resultats = Object.entries(snapshot.val())
+      ClearList();
+      for(let i = 0; i < resultats.length; i++) {
+            let current = resultats[i]
+            addElement(current)
+      }}else{
+            lista.innerHTML = "Buy something"
+      }
 })
-
